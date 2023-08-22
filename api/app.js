120bab4bc0de10637e7d4d8c97a7b3a66b544a5f
Ethan Szeto <ethan.szeto@gmail.com>
2023-08-22 15:18:26 -0400

    Started adding session information in the app

--- api/app.js
+++ api/app.js
@@ -5,6 +5,9 @@ import pagesRouter from "./routes/pages.route.js";
 import articlesRouter from "./routes/articles.route.js";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import sessions from "express-session";
+import MongoStore from "connect-mongo";
 import auth from "./authenticator/authentication.js";
 //import articles from './routes/articles.route.js'
 
@@ -21,6 +24,7 @@ app.set("view engine", "ejs");
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan("tiny"));
 
 app.use(bodyParser.json());
@@ -30,9 +34,36 @@ app.use("/", pagesRouter);
 
 app.use("/articles", articlesRouter);
 
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_CLUSTER } = process.env;
+const DATABASE_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_CLUSTER}.xtdufxk.mongodb.net/?retryWrites=true&w=majority`;
+
+app.use(sessions({
+    store: MongoStore.create(DATABASE_URL),
+    secret: process.env.TOKEN_KEY,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 5 }, //5 minutes session
+    resave: false
+}));
+
+app.get('/logout',(req,res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
 //Need to figure out how to make this compatinle with Express Router Object
 app.get("/profile", auth, (req, res) => {
-    res.render('profile', {loggedIn: "yes"});
+    session = req.session;
+    if(session.userid) {
+        res.render('profile', {loggedIn: "yes"});
+    } else {
+        res.json({LoggedIn: "nah"});
+    }
+    
   });
+
+  /*
+   * Need to figure out how to utilize the existing authentication 
+   * for this user session model.
+   */
 
 export default app;
